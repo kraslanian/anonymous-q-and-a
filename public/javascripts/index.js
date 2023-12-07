3240efdb71da936595c1ebcf9c7628d0137fefb5
Kristina <ks6304@nyu.edu>
2023-12-06 22:56:44 -0500

    done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,18 +1,164 @@
-// helper function for creating elements (usage optional)
-function createElement(type, attrs, ...children) {
-  const ele = document.createElement(type);
+document.addEventListener('DOMContentLoaded', main);
 
-  // add element attributes
-  for (const prop in attrs) {
-    if (attrs.hasOwnProperty(prop)) {
-      ele.setAttribute(prop, attrs[prop]);
-    }
-  }
-
-  // add child nodes to element
-  children.forEach(c => ele.appendChild(typeof c === 'string' ? document.createTextNode(c) : c));
-
-  return ele;
+function main() {
+  fetchData();
+  setEventListeners();
 }
 
-// TODO: finish client side javascript
+function setEventListeners() {
+  const askQuestionButton = document.querySelector('#btn-show-modal-question');
+  askQuestionButton.addEventListener('click', openQuestionModal);
+
+  const modalCloseButtons = document.querySelectorAll('.close-modal');
+  modalCloseButtons.forEach(button => button.addEventListener('click', closeModal));
+}
+
+function openQuestionModal() {
+  const questionModal = document.getElementById('modal-question');
+  questionModal.style.display = 'block';
+
+  const closeBtn = document.getElementById('close_question');
+  closeBtn.addEventListener('click', closeModal);
+
+  const askButton = document.getElementById('create-question');
+  askButton.addEventListener('click', handleQuestionSubmission);
+}
+
+function openAnswerModal(objectId) {
+  const answerModal = document.getElementById('modal-answer');
+  answerModal.style.display = 'block';
+
+  const closeBtn = document.getElementById('close_answer');
+  closeBtn.addEventListener('click', closeModal);
+
+  const submitButton = document.getElementById('create-answer');
+  submitButton.addEventListener('click', () => {
+    const newAnswer = document.getElementById('answer-text').value;
+    postNewAnswer(objectId, newAnswer);
+  });
+}
+
+function closeModal() {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => (modal.style.display = 'none'));
+}
+
+function handleQuestionSubmission() {
+  const questionText = document.getElementById('question-text').value;
+  postNewQuestion(questionText);
+}
+
+function postNewQuestion(newQuestion) {
+  closeModal();
+  const data = { question: newQuestion };
+  const jsonData = JSON.stringify(data);
+  const postApiUrl = 'http://localhost:3000/questions/';
+
+  fetchAndReload(postApiUrl, jsonData);
+}
+
+function postNewAnswer(objectId, newAnswer) {
+  closeModal();
+  const data = { id: objectId, answer: newAnswer };
+  const jsonData = JSON.stringify(data);
+  const postAnswerUrl = `http://localhost:3000/questions/${objectId}/answers/`;
+
+  fetchAndReload(postAnswerUrl, jsonData);
+}
+
+function fetchAndReload(url, data) {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: data,
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(() => window.location.reload())
+    .catch(error => console.error('Error:', error));
+}
+
+function fetchData() {
+  const apiUrl = 'http://localhost:3000/questions';
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => renderQuestionsAndAnswers(data))
+    .catch(error => console.error('Error:', error));
+}
+
+function renderQuestionsAndAnswers(data) {
+  const qandaContainer = document.createElement('div');
+  qandaContainer.className = 'qanda-container';
+
+  data.forEach(item => {
+    const { _id, question, answers } = item;
+    const questionElement = createQuestionElement(question);
+    const answerElements = createAnswerElements(answers);
+    const submitBtnContainer = createSubmitBtnContainer(_id);
+    const questionContainer = createQuestionContainer(questionElement, answerElements, submitBtnContainer);
+    qandaContainer.appendChild(questionContainer);
+  });
+
+  document.body.appendChild(qandaContainer);
+}
+
+function createQuestionElement(text) {
+  const questionElement = document.createElement('h2');
+  questionElement.className = 'question';
+  questionElement.textContent = text;
+  return questionElement;
+}
+
+function createAnswerElements(answers) {
+  return answers.map(answer => {
+    const answerElement = document.createElement('p');
+    answerElement.className = 'answer';
+    answerElement.textContent = answer;
+    return answerElement;
+  });
+}
+
+function createSubmitBtnContainer(objectId) {
+  const submitBtnContainer = document.createElement('div');
+  submitBtnContainer.className = 'submit-btn-container';
+
+  const submitButton = createButton('Submit Another Answer', 'submit-button', () => {
+    const objectIdValue = submitButton.nextSibling.textContent;
+    openAnswerModal(objectIdValue);
+  });
+
+  submitBtnContainer.appendChild(submitButton);
+
+  const objectIdElement = document.createElement('div');
+  objectIdElement.textContent = objectId;
+  objectIdElement.style.display = 'none';
+
+  submitBtnContainer.appendChild(objectIdElement);
+
+  return submitBtnContainer;
+}
+
+function createQuestionContainer(questionElement, answerElements, submitBtnContainer) {
+  const questionContainer = document.createElement('div');
+  questionContainer.className = 'question-container';
+  questionContainer.appendChild(questionElement);
+
+  answerElements.forEach(answerElement => questionContainer.appendChild(answerElement));
+
+  questionContainer.appendChild(submitBtnContainer);
+  return questionContainer;
+}
+
+function createButton(text, className, clickHandler) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  button.className = className;
+  button.addEventListener('click', clickHandler);
+  return button;
+}
--- src/app.mjs
+++ src/app.mjs
@@ -4,6 +4,7 @@ import express from 'express'
 import Question from './db.mjs'
 import url from 'url'
 import path from 'path'
+import bodyParser from 'body-parser'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
@@ -11,24 +12,45 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 app.post('/questions/', async (req, res) => {
-  // TODO: finish implementation
-})
+  try {
+    const { question } = req.body;
+
+    const newQuestion = new Question({
+      question: question,
+      answers: [] 
+    });
+    const savedQuestion = await newQuestion.save();
+    res.json(savedQuestion); 
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Failed to add question' });
+  }
+});
 
 app.post('/questions/:id/answers/', async (req, res) => {
+  
+  console.log(req.body);
   const update = { "$push": { answers: req.body.answer } }
   try {
     const result = await Question.findByIdAndUpdate(req.params.id, update, { "new": true })
     res.json({ success: 'Added an answer' })
-  } catch(e) {
+  } catch(error) {
     res.status(500).json({ error: 'Failed to add answer' })
   }
 })
 
 app.get('/questions/', async (req, res) => {
-  // TODO: finish implementation
-})
+  try {
+    const questions = await Question.find();
+    res.json(questions);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve questions' });
+  }
+});
 
 
 const port = process.env.PORT || 3000
